8d3826c3376ff45c6cd9968da009d815ec78f9ea
Flexan <flexangaming@gmail.com>
2026-07-20 20:27:09 +0200

    Update LayeredForm.cs

--- LayeredForm.cs
+++ LayeredForm.cs
@@ -3,7 +3,7 @@ using System.Drawing;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
-namespace RobotInterfaceUI
+namespace SmolFlexan
 {
     public class LayeredForm : Form
     {
